e24b3b63807f5718f972df5f8450028c25376337
VIJAYASRI08 <vijayasrie23@gmail.com>
2024-08-21 00:02:27 +0530

    Create App.js

---
+++ loadprediction/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Topbar/Header';
+import Pageend from './Footer/Pageend';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div>
+      </div>
+      <Pageend/>
+    </div>
+  );
+}
+
+export default App;
+
